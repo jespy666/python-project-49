e88da89204db287ebc55e6266c6ef124aa7689c4
Georgievsky <jespy.kz@gmail.com>
2023-02-14 15:06:16 +0600

    add game "brain-progression" by step 8

---
+++ brain_games/games/brain_progression.py
@@ -0,0 +1,31 @@
+from random import randint
+
+
+exercise = 'What number is missing in the progression?'
+
+
+def start_game():
+    question, answer = choice_symbol()
+    return question, str(answer)
+
+
+def generate_subsequence():
+    i = randint(1, 10)
+    step = randint(2, 5)
+    subsequence = []
+    while len(subsequence) <= 10:
+        subsequence.append(i)
+        i += step
+    return subsequence
+
+
+def choice_symbol():
+    sequence = generate_subsequence()
+    hidden_symbol = randint(0, 9)
+    answer = sequence[hidden_symbol]
+    sequence[hidden_symbol] = '..'
+    result = ''
+    for j in sequence:
+        result += str(j)
+        result += ' '
+    return result[:-1], answer
